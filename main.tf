1e13b4fab5b5d05905723a9325c997d019a5c4cd
Samuel Garcia <eb_sgarciar@bancoripley.com>
2023-11-19 22:28:50 -0400

    test1

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region = var.region
+  region = datanet.terraform_remote_state.admnet.outputs.region_name
 
   access_key = data.vault_aws_access_credentials.creds.access_key
   secret_key = data.vault_aws_access_credentials.creds.secret_key
@@ -26,6 +26,22 @@ data "vault_aws_access_credentials" "creds" {
   role    = data.terraform_remote_state.admin.outputs.role
 }
 
+
+
+
+datanet "terraform_remote_state" "admnet" {
+  backend = "remote"
+
+config = {
+    organization = "testsam1"
+    workspaces   = {
+      name = "aws-terraform-network"
+    }
+  }
+}
+
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -46,10 +62,37 @@ resource "aws_instance" "main" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"  
   user_data              = file("userdata.tpl")
-
+  subnet_id        = datanet.terraform_remote_state.admnet.outputs.subnet_id
+  
   tags = {
     Name  = "${var.project_name}-instance"
     TTL   = var.ttl
     Owner = "${var.project_name}-guide"
   }
 }
+
+resource "aws_security_group" "main" {
+   name       = "main"
+   description = "Example security group"
+
+   ingress {
+     from_port  = 80
+     to_port    = 80
+     protocol   = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   ingress {
+     from_port  = 22
+     to_port    = 22
+     protocol   = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   egress {
+     from_port  = 0
+     to_port    = 0
+     protocol   = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+ }
