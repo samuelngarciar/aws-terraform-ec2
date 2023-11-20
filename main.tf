66f8cf70448b68a590a410ef1fd1afcbf7e3322f
samuelngarciar <84947793+samuelngarciar@users.noreply.github.com>
2023-11-20 00:07:06 -0400

    using rolenet

--- main.tf
+++ main.tf
@@ -20,7 +20,8 @@ config = {
 
 data "vault_aws_access_credentials" "creds" {
   backend = data.terraform_remote_state.admin.outputs.backend
-  role    = data.terraform_remote_state.admin.outputs.role
+  #role    = data.terraform_remote_state.admin.outputs.role
+  role    = data.terraform_remote_state.admin.outputs.rolenet
 }
 
 
