be343d5875e5d7f06a621c2ec24b6e041b2f35fe
samuelngarciar <84947793+samuelngarciar@users.noreply.github.com>
2023-11-19 23:35:00 -0400

    sin security group

--- main.tf
+++ main.tf
@@ -55,40 +55,14 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-resource "aws_security_group" "ec2" {
-   name       = "${var.project_name}-sg"
-   description = "the security group"
-   vpc_id      = "${data.terraform_remote_state.admnet.outputs.vpc_id}"
-  
 
-   ingress {
-     from_port  = 80
-     to_port    = 80
-     protocol   = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
-
-   ingress {
-     from_port  = 22
-     to_port    = 22
-     protocol   = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
-
-   egress {
-     from_port  = 0
-     to_port    = 0
-     protocol   = "-1"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
- }
 
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"  
   #user_data    = file("userdata.tpl")
   subnet_id     = data.terraform_remote_state.admnet.outputs.subnet_id
-  vpc_security_group_ids = [aws_security_group.ec2.id]
+  #vpc_security_group_ids = [aws_security_group.ec2.id]
   
   tags = {
     Name  = "${var.project_name}-instance"
